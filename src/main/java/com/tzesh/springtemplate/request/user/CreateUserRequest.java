aaff69b1491110779c788a37f3588ed98cf15e27
Tzesh <mrtzesh@gmail.com>
2023-05-15 18:54:16 +0300

    added jakarta validation handling mechanism

--- src/main/java/com/tzesh/springtemplate/request/user/CreateUserRequest.java
+++ src/main/java/com/tzesh/springtemplate/request/user/CreateUserRequest.java
@@ -2,16 +2,16 @@ package com.tzesh.springtemplate.request.user;
 
 import com.tzesh.springtemplate.enums.auth.RoleEnum;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 /**
  * @author tzesh
  */
 public record CreateUserRequest(
-        @NotNull @Length(min = 3, max = 50) String username,
-        @NotNull @Length(min = 8, max = 128) String password,
-        @NotNull @Email String email,
-        @NotNull @Length(min = 3, max = 50) String name,
-        @NotNull RoleEnum role)
+        @NotEmpty @Length(min = 3, max = 50) String username,
+        @NotEmpty @Length(min = 8, max = 128) String password,
+        @NotEmpty @Email String email,
+        @NotEmpty @Length(min = 3, max = 50) String name,
+        @NotEmpty RoleEnum role)
 { }
